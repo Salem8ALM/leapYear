bcb0f3bbef0f44d1f30e13ea515bdfbe3f8d7b1c
SALEM .. <salemsnsalmutairi@gmail.com>
2024-11-06 11:28:18 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a year");
+
+        int input = scanner.nextInt();
+
+        if ((input%4 == 0 && input%100 !=0) || input%400 ==0 ){
+            System.out.println("you've entered a leap year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+}
